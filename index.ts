72a880d3e7cd54bb106b780c594726f2ca654aef
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-12-20 14:50:08 +0800

    9.9 PORT changed to 3001

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
     console.log('Someone pinged here!');
