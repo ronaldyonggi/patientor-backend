3cb02c4bf16c94e3e46117d8d3835b214ac78a7f
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-12-25 18:45:10 +0800

    9.12 add POST end point for adding patient

--- src/routes/patients.ts
+++ src/routes/patients.ts
@@ -7,5 +7,19 @@ router.get('/', (_req, res) => {
     res.send(patientService.getNonSSNPatients());
 });
 
+router.post('/', (req, res) => {
+    try {
+        const { name, dateOfBirth, ssn, gender, occupation } = req.body;
+        const addedEntry = patientService.addPatient(name, dateOfBirth, ssn, gender, occupation);
+        res.json(addedEntry);
+    } catch(error: unknown) {
+        let errorMessage = 'Something went wrong.';
+        if (error instanceof Error) {
+            errorMessage += ' Error: ' + error.message;
+        }
+        res.status(400).send(errorMessage);
+    }
+})
+
 export default router;
 
