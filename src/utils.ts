718dfeffd1cfb4c9de2a006f1d7aaf0f6238c9e8
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-01-01 13:16:18 +0800

    9.20 add entries property to new patient object

--- src/utils.ts
+++ src/utils.ts
@@ -68,6 +68,7 @@ const toNewPatient = (object: unknown): NewPatient => {
             ssn: parseSSN(object.ssn),
             gender: parseGender(object.gender),
             occupation: parseOccupation(object.occupation),
+            entries: []
         };
 
         return newPatient;
